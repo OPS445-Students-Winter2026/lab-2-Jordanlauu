8201fc9c238b1087357c8d7fd10c2186d20a84ad
Jordan Lau <jlau103@myseneca.ca>
2026-01-28 16:53:03 -0500

    added lab2e

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#Author: Jordan Lau
+#AuthorID: 144424249
+#Date Created: 2026/01/20
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
